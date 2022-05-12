a703fb43c908226560cdfd8b3df456cfeaee4769
ander <ander@DESKTOP-G3BCR3T>
2022-05-12 14:49:05 -0300

    Primeiras edições objetos.

--- github.com/andersonlenzjava/programa.java.objetos.1.git/programa-java-objetos-1/src/curso/java/objetos/one/ClassePrincipal.java
+++ github.com/andersonlenzjava/programa.java.objetos.1.git/programa-java-objetos-1/src/curso/java/objetos/one/ClassePrincipal.java
@@ -1,5 +1,19 @@
 package curso.java.objetos.one;
 
+import cursojava.classes.Aluno;
+
 public class ClassePrincipal {
+
+	public static void main(String[] args) {
+
+		/* Criação de um objeto na memória com o endereço dentro da variável aluno1 */
+		Aluno aluno1 = new Aluno();
+		Aluno aluno2 = new Aluno();	
+		Aluno aluno3 = new Aluno("Jose", 55);
+		
+		aluno1.setDataMatricula("03/09/1992");
+		
+		System.out.println("Data da matricula: " + aluno1.getDataMatricula());
+	}
 
 }
